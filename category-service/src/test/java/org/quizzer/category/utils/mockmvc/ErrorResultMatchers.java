929a12bcbcea5224b0ebce0df5139032cde37635
dawid <daw.kaluza@gmail.com>
2021-04-19 18:24:30 +0200

    Improve methods

--- category-service/src/test/java/org/quizzer/category/utils/mockmvc/ErrorResultMatchers.java
+++ category-service/src/test/java/org/quizzer/category/utils/mockmvc/ErrorResultMatchers.java
@@ -1,12 +1,17 @@
 package org.quizzer.category.utils.mockmvc;
 
+import org.hamcrest.text.MatchesPattern;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.regex.Pattern;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public final class ErrorResultMatchers {
+    private static final Pattern iso8601Pattern = Pattern.compile("^(\\d{4}-\\d{2}-\\d{2})[A-Z]+(\\d{2}:\\d{2}:\\d{2}).([0-9+-:]+)$");
+
     private ErrorResultMatchers() {
     }
 
@@ -15,6 +20,12 @@ public final class ErrorResultMatchers {
             .andExpect(status().is(status.value()))
             .andExpect(jsonPath("$.status").value(status.value()))
             .andExpect(jsonPath("$.message").exists())
-            .andExpect(jsonPath("$.timestamp").exists());
+            .andExpect(jsonPath("$.timestamp").value(MatchesPattern.matchesPattern(iso8601Pattern)));
+    }
+
+    public static void expectFieldError(ResultActions resultActions, String field) throws Exception {
+        resultActions
+            .andExpect(jsonPath("$.fieldErrors[?(@.field == '" + field + "')].field").exists())
+            .andExpect(jsonPath("$.fieldErrors[?(@.field == '" + field + "')].message").exists());
     }
 }
