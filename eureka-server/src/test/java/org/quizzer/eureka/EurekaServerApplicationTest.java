c134a4f54df4a1dcf484ecb79f6efdbbe8147b70
Dawid Kałuża <daw.kaluza@gmail.com>
2021-05-05 20:30:41 +0200

    Complete eureka server with docker

---
+++ eureka-server/src/test/java/org/quizzer/eureka/EurekaServerApplicationTest.java
@@ -0,0 +1,12 @@
+package org.quizzer.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaServerApplicationTest {
+    @Test
+    public void contextStarts() {
+
+    }
+}
